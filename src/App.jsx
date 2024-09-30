c0dca9b6a1f62366b5753a835bab7755f8f07795
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-30 17:50:40 +0100

    feat: include the desktop pattern image for the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import patternDesktop from "./images/pattern-bg-desktop.png";
+
 function App() {
-  return <div></div>;
+  return (
+    <main>
+      <section className="flex items-center justify-center">
+        <img src={patternDesktop} alt="desktop background" className="w-full" />
+      </section>
+    </main>
+  );
 }
 
 export default App;
