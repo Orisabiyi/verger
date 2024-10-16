98df8b3aec13433fba12cdc8d72e7dbfd122d46c
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-16 06:16:51 +0100

    feat: setup route to DashboardMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Route } from "react-router-dom";
 import { BrowserRouter, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import DashboardMenu from "./pages/DashboardMenu";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<DashboardMenu />} />
+          <Route path="dashboard" element={<DashboardMenu />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
