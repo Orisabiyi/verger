49cb0c382d2a1a49a993cdcfd0124cf2467c5a0b
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-16 05:47:44 +0100

    style: style the dashboard list

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,46 @@
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 import verger from "../assets/verger-logo.svg";
+import dashIcon from "../assets/dash-icon.svg";
+import verifyIcon from "../assets/verify-icon.svg";
+import assetIcon from "../assets/asset-icon.svg";
+import doorIcon from "../assets/door-01.svg";
 
 function Dashboard() {
   return (
-    <main>
-      <aside>
-        <figure>
+    <main className="flex items-stretch min-h-screen">
+      <aside className="flex flex-col items-start justify-start w-1/5 py-20 pl-20 bg-primary">
+        <figure className="mb-24">
           <img src={verger} alt="verger logo icon" />
         </figure>
+
+        <ul className="flex flex-col gap-8">
+          <li>
+            <Link className="flex items-center gap-3">
+              <img src={dashIcon} alt="icon for dashboard" />
+              <span>Dashboard</span>
+            </Link>
+          </li>
+
+          <li>
+            <Link className="flex items-center gap-3">
+              <img src={verifyIcon} alt="icon for dashboard" />
+              <span>Verify Item</span>
+            </Link>
+          </li>
+
+          <li>
+            <Link className="flex items-center gap-3">
+              <img src={assetIcon} alt="icon for dashboard" />
+              <span>My Assets</span>
+            </Link>
+          </li>
+
+          <li></li>
+        </ul>
       </aside>
+
+      <Outlet />
     </main>
   );
 }
