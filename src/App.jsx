adac5dc9e8f1cd05051081e4bd9f24f14b86e370
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-16 04:56:55 +0100

    feat: setup routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
+import { BrowserRouter, Routes } from "react-router-dom";
 import patternDesktop from "./images/pattern-bg-desktop.png";
 
 function App() {
   return (
-    <main>
-      <section className="relative overflow-hidden bg-gray-400">
-        <img
-          src={patternDesktop}
-          alt="desktop background"
-          className="absolute top-0 left-0"
-        />
-        <h1>Track IP</h1>
-      </section>
-    </main>
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
   );
 }
 
