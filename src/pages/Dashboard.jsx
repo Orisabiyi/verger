c8196f7349f06681c0aceed5e16ad0e610e0daec
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-16 05:08:55 +0100

    chores: include the logo for the page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,15 @@
+import verger from "../assets/verger-logo.svg";
+
 function Dashboard() {
-  return <main></main>;
+  return (
+    <main>
+      <aside>
+        <figure>
+          <img src={verger} alt="verger logo icon" />
+        </figure>
+      </aside>
+    </main>
+  );
 }
 
 export default Dashboard;
