044ec9cdc129f4679de86359d3a898fd145a3d56
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-16 14:16:46 +0100

    style: complete the cards for items

--- src/pages/DashboardMenu.jsx
+++ src/pages/DashboardMenu.jsx
@@ -81,6 +81,124 @@ function DashboardMenu() {
           Check
         </button>
       </form>
+
+      <div>
+        <div className="flex items-center justify-between mb-8">
+          <h2 className="font-semibold text-28">Your Items</h2>
+
+          <button className="flex items-center justify-center gap-2 px-8 py-2 bg-white border-2 rounded-full text-12">
+            <span>Add new Item</span>
+            <span>+</span>
+          </button>
+        </div>
+
+        <div className="flex items-stretch gap-8 text-12 text-cta">
+          <article className="flex flex-col items-center w-1/3 gap-5 px-4 py-4 bg-secondary bg-opacity-20 rounded-3xl">
+            <figure className="w-32 bg-white h-19 rounded-2xl"></figure>
+
+            <div className="flex items-center justify-between w-full text-13">
+              <h4 className="font-semibold">iPhone 16 Pro Max</h4>
+              <button className="px-16 py-2 text-white rounded-full bg-cta">
+                Status: Legit
+              </button>
+            </div>
+
+            <div className="grid w-full grid-cols-3 gap-4">
+              <p className="col-span-3">
+                Blockchain ID: 0x8f29e7bf6627A9D85d3212c
+              </p>
+              <p className="col-span-3 mb-4">Product ID: 27A9D85d3212c</p>
+
+              <p className="col-span-1 font-semibold">Date Created:</p>
+              <p className="col-span-2">11th October, 2024</p>
+
+              <p className="col-span-1 font-semibold">Location:</p>
+              <p className="col-span-2">Shenzen, China</p>
+
+              <p className="col-span-1 font-semibold">Previous Owners:</p>
+              <p className="col-span-2">Owner 1 - 0x8f29e7...A9D85212c</p>
+
+              <button className="w-1/2 col-span-3 col-start-1 border-2 rounded-full border-secondary ml-44">
+                Ownership History
+              </button>
+            </div>
+
+            <button className="w-full py-2 text-white bg-cta rounded-xl text-13">
+              Product Details
+            </button>
+          </article>
+
+          <article className="flex flex-col items-center w-1/3 gap-5 px-4 py-4 bg-secondary bg-opacity-20 rounded-3xl">
+            <figure className="w-32 bg-white h-19 rounded-2xl"></figure>
+
+            <div className="flex items-center justify-between w-full text-13">
+              <h4 className="font-semibold">iPhone 16 Pro Max</h4>
+              <button className="px-16 py-2 text-white rounded-full bg-cta">
+                Status: Legit
+              </button>
+            </div>
+
+            <div className="grid w-full grid-cols-3 gap-4">
+              <p className="col-span-3">
+                Blockchain ID: 0x8f29e7bf6627A9D85d3212c
+              </p>
+              <p className="col-span-3 mb-4">Product ID: 27A9D85d3212c</p>
+
+              <p className="col-span-1 font-semibold">Date Created:</p>
+              <p className="col-span-2">11th October, 2024</p>
+
+              <p className="col-span-1 font-semibold">Location:</p>
+              <p className="col-span-2">Shenzen, China</p>
+
+              <p className="col-span-1 font-semibold">Previous Owners:</p>
+              <p className="col-span-2">Owner 1 - 0x8f29e7...A9D85212c</p>
+
+              <button className="w-1/2 col-span-3 col-start-1 border-2 rounded-full border-secondary ml-44">
+                Ownership History
+              </button>
+            </div>
+
+            <button className="w-full py-2 text-white bg-cta rounded-xl text-13">
+              Product Details
+            </button>
+          </article>
+
+          <article className="flex flex-col items-center w-1/3 gap-5 px-4 py-4 bg-secondary bg-opacity-20 rounded-3xl">
+            <figure className="w-32 bg-white h-19 rounded-2xl"></figure>
+
+            <div className="flex items-center justify-between w-full text-13">
+              <h4 className="font-semibold">iPhone 16 Pro Max</h4>
+              <button className="px-16 py-2 text-white rounded-full bg-cta">
+                Status: Legit
+              </button>
+            </div>
+
+            <div className="grid w-full grid-cols-3 gap-4">
+              <p className="col-span-3">
+                Blockchain ID: 0x8f29e7bf6627A9D85d3212c
+              </p>
+              <p className="col-span-3 mb-4">Product ID: 27A9D85d3212c</p>
+
+              <p className="col-span-1 font-semibold">Date Created:</p>
+              <p className="col-span-2">11th October, 2024</p>
+
+              <p className="col-span-1 font-semibold">Location:</p>
+              <p className="col-span-2">Shenzen, China</p>
+
+              <p className="col-span-1 font-semibold">Previous Owners:</p>
+              <p className="col-span-2">Owner 1 - 0x8f29e7...A9D85212c</p>
+
+              <button className="w-1/2 col-span-3 col-start-1 border-2 rounded-full border-secondary ml-44">
+                Ownership History
+              </button>
+            </div>
+
+            <button className="w-full py-2 text-white bg-cta rounded-xl text-13">
+              Product Details
+            </button>
+          </article>
+        </div>
+      </div>
     </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,18 @@ export default {
       },
       fontSize: {
         12: "1.2rem",
+        13: "1.3rem",
         16: "1.6rem",
         28: "2.8rem",
       },
       padding: {
         50: "13rem",
+      },
+      width: {
+        32: "32rem",
+      },
+      height: {
+        19: "19rem",
       },
     },
   },
