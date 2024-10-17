bd2304013a55e681ccf622bad4a09737eb61d1dd
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-17 15:54:48 +0100

    feat: create section for product details

--- src/pages/VerifyItem.jsx
+++ src/pages/VerifyItem.jsx
@@ -1,6 +1,6 @@
 function VerifyItem() {
   return (
-    <section className="flex flex-col items-center flex-1 gap-2 py-20 bg-opacity-30 bg-primary">
+    <section className="flex flex-col items-center flex-1 gap-2 px-10 py-20 bg-opacity-30 bg-primary">
       <h1 className="mb-20 font-semibold text-center text-28 text-cta">
         Product Verification
       </h1>
@@ -19,6 +19,44 @@ function VerifyItem() {
         Type your product/item ID in the input field above to verify the
         authenticity of your item
       </p>
+
+      <h2 className="self-start mt-32 font-semibold text-28">
+        Product Details
+      </h2>
+
+      <article className="flex items-center self-start w-full gap-8 mt-8 text-16">
+        <figure className="w-4/12 bg-white h-19 rounded-2xl"></figure>
+        <ul className="grid flex-1 grid-cols-2 gap-2">
+          <li>
+            <span className="inline-block w-4 h-4 mr-2 rounded-full bg-cta"></span>
+            <b>Date Created: </b> 11th October, 2024
+          </li>
+          <li>
+            <span className="inline-block w-4 h-4 mr-2 rounded-full bg-cta"></span>
+            <b>Registered By: </b> Emmanuel Godfrey
+          </li>
+          <li>
+            <span className="inline-block w-4 h-4 mr-2 rounded-full bg-cta"></span>
+            <b>Quality check: </b> Certified Original
+          </li>
+          <li>
+            <span className="inline-block w-4 h-4 mr-2 rounded-full bg-cta"></span>
+            <b>Ownership: </b> Transferred
+          </li>
+          <li>
+            <span className="inline-block w-4 h-4 mr-2 rounded-full bg-cta"></span>
+            <b>Current Owner: </b> Leumas Okechukwu
+          </li>
+          <li>
+            <span className="inline-block w-4 h-4 mr-2 rounded-full bg-cta"></span>
+            <b>Dispute: </b> None
+          </li>
+
+          <button className="col-span-2 px-10 py-4 mt-8 font-semibold text-white bg-cta rounded-2xl">
+            More Details
+          </button>
+        </ul>
+      </article>
     </section>
   );
 }
