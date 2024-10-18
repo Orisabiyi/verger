e3ad8438b0767bdfc27274a0b71ad2653b602b89
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-18 12:56:46 +0100

    refactor: refactor ui code styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,7 +8,7 @@ import doorIcon from "../assets/door-01.svg";
 
 function Dashboard() {
   return (
-    <main className="relative flex items-stretch min-h-screen">
+    <main className="relative flex h-screen">
       <aside className="flex flex-col justify-start w-1/5 py-20 pl-20 pr-4 bg-primary">
         <figure className="mb-24">
           <img src={verger} alt="verger logo icon" />
@@ -49,7 +49,7 @@ function Dashboard() {
         </ul>
       </aside>
 
-      <Outlet />
+      {/* <Outlet /> */}
     </main>
   );
 }
