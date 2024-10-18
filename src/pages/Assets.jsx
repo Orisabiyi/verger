33a7b64ca93781d3c860200096c5512a91397343
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-18 14:12:34 +0100

    style: change the padding of My asset dashboard screen and scrolling for Assets

--- src/pages/Assets.jsx
+++ src/pages/Assets.jsx
@@ -1,6 +1,6 @@
 function Assets() {
   return (
-    <section className="flex flex-col items-center flex-1 gap-2 px-10 py-20 bg-opacity-30 bg-primary">
+    <section className="flex flex-col items-center flex-1 gap-2 px-5 py-20 bg-opacity-30 bg-primary overflow-y-auto">
       <h1 className="mb-20 font-semibold text-center text-28 text-cta">
         My Assets
       </h1>
