6729c7de60a973367164dda2ab1099030b499078
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-21 09:31:20 +0100

    feat: setup the config for firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAx7ZIeniIGPKeVXx9RBWz0IMcjUCWPc8",
+  authDomain: "verger-alx.firebaseapp.com",
+  projectId: "verger-alx",
+  storageBucket: "verger-alx.appspot.com",
+  messagingSenderId: "204037937986",
+  appId: "1:204037937986:web:66c3072343ac77d85386db",
+  measurementId: "G-5P8TFG7WFY",
+};
+
+const app = initializeApp(firebaseConfig);
