2324516a378d2995a9c79853dc9a6a6ac952a4f9
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-25 15:38:14 +0100

    refactor: Update product status display and link in DashboardMenu

--- src/pages/Assets.jsx
+++ src/pages/Assets.jsx
@@ -50,60 +50,72 @@ function Assets() {
 
         <div className="grid grid-cols-3 items-stretch gap-8 text-12 text-cta">
           {products &&
-            products.map((product, i) => (
-              <article
-                className="flex flex-col items-center gap-5 px-4 py-4 bg-secondary bg-opacity-20 rounded-3xl"
-                key={i}
-              >
-                <figure className="w-35 bg-white h-19 rounded-2xl">
-                  <img
-                    src={product.productImage}
-                    alt={`${product.productName} image`}
-                    className="w-full h-full rounded-2xl"
-                  />
-                </figure>
+            products.map(
+              (product, i) =>
+                product.status === "success" && (
+                  <article
+                    className="flex flex-col items-center gap-5 px-4 py-4 bg-secondary bg-opacity-20 rounded-3xl"
+                    key={i}
+                  >
+                    <figure className="w-35 bg-white h-19 rounded-2xl">
+                      <img
+                        src={product.productImage}
+                        alt={`${product.productName} image`}
+                        className="w-full h-full rounded-2xl"
+                      />
+                    </figure>
 
-                <div className="flex items-center justify-between w-full text-13">
-                  <h4 className="font-semibold">{product.productName}</h4>
-                  <button className="px-16 py-2 text-white rounded-full bg-cta">
-                    Status: {product.status === "abort_by_response" && "abort"}
-                    {product.status === "success" && "success"}
-                  </button>
-                </div>
+                    <div className="flex items-center justify-between w-full text-13">
+                      <h4 className="font-semibold">{product.productName}</h4>
+                      <button className="px-16 py-2 text-white rounded-full bg-cta">
+                        Status:{" "}
+                        {product.status === "abort_by_response" && "abort"}
+                        {product.status === "success" && "success"}
+                      </button>
+                    </div>
 
-                <div className="grid w-full grid-cols-3 gap-4">
-                  <p className="col-span-3">
-                    Blockchain ID: {product.blockchainId.slice(0, 16) + "....."}
-                  </p>
-                  <p className="col-span-3 mb-4">
-                    Product ID: {product.productId}
-                  </p>
+                    <div className="grid w-full grid-cols-3 gap-4">
+                      <p className="col-span-3">
+                        Blockchain ID:{" "}
+                        {product.blockchainId.slice(0, 16) + "....."}
+                      </p>
+                      <p className="col-span-3 mb-4">
+                        Product ID: {product.productId}
+                      </p>
 
-                  <p className="col-span-1 font-semibold">Date Created:</p>
-                  <p className="col-span-2">
-                    {new Date(product.createdAt).toLocaleDateString("en-US", {
-                      day: "numeric",
-                      month: "long",
-                      year: "numeric",
-                    })}
-                  </p>
+                      <p className="col-span-1 font-semibold">Date Created:</p>
+                      <p className="col-span-2">
+                        {new Date(product.createdAt).toLocaleDateString(
+                          "en-US",
+                          {
+                            day: "numeric",
+                            month: "long",
+                            year: "numeric",
+                          }
+                        )}
+                      </p>
 
-                  <p className="col-span-1 font-semibold">Previous Owners:</p>
-                  <p className="col-span-2">Owner 1 - 0x8f29e7...A9D85212c</p>
+                      <p className="col-span-1 font-semibold">
+                        Previous Owners:
+                      </p>
+                      <p className="col-span-2">
+                        Owner 1 - 0x8f29e7...A9D85212c
+                      </p>
 
-                  <button className="w-1/2 col-span-3 col-start-1 border-2 rounded-full border-secondary ml-44">
-                    Ownership History
-                  </button>
-                </div>
+                      <button className="w-1/2 col-span-3 col-start-1 border-2 rounded-full border-secondary ml-44">
+                        Ownership History
+                      </button>
+                    </div>
 
-                <Link
-                  to={`/verify-item/${product.productId}`}
-                  className="w-full py-2 text-white text-center bg-cta rounded-xl text-13"
-                >
-                  Product Details
-                </Link>
-              </article>
-            ))}
+                    <Link
+                      to={`/verify-item/${product.productId}`}
+                      className="w-full py-2 text-white text-center bg-cta rounded-xl text-13"
+                    >
+                      Product Details
+                    </Link>
+                  </article>
+                )
+            )}
         </div>
       </div>
     </section>
