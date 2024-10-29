be0b22be9123fab004412640e9675cc77bb41641
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-29 11:53:14 +0100

    style: implement the highlighting of the active page option in the aside component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,8 +5,11 @@ import verger from "../assets/verger-logo.svg";
 import dashIcon from "../assets/dash-icon.svg";
 import verifyIcon from "../assets/verify-icon.svg";
 import assetIcon from "../assets/asset-icon.svg";
+import { useLocation } from "react-router-dom";
 
 function Dashboard() {
+  const { pathname } = useLocation();
+
   return (
     <main className="flex h-screen overflow-hidden">
       <aside className="flex flex-col justify-start w-80 py-20 pl-10 pr-4 bg-primary">
@@ -18,7 +21,9 @@ function Dashboard() {
           <li>
             <Link
               to="/user/dashboard"
-              className="flex items-center w-full gap-3 px-4 py-6 transition-colors duration-500 rounded-xl hover:bg-secondary"
+              className={`flex items-center w-full gap-3 px-4 py-6 transition-colors duration-500 rounded-xl hover:bg-secondary ${
+                pathname.includes("dashboard") ? "bg-secondary" : ""
+              }`}
             >
               <img src={dashIcon} alt="icon for dashboard" />
               <span>Dashboard</span>
@@ -28,7 +33,9 @@ function Dashboard() {
           <li>
             <Link
               to="/user/verify-item"
-              className="flex items-center w-full gap-3 px-4 py-6 transition-colors duration-500 rounded-xl hover:bg-secondary"
+              className={`flex items-center w-full gap-3 px-4 py-6 transition-colors duration-500 rounded-xl hover:bg-secondary ${
+                pathname.includes("verify-item") ? "bg-secondary" : ""
+              }`}
             >
               <img src={verifyIcon} alt="icon for dashboard" />
               <span>Verify Item</span>
@@ -38,7 +45,9 @@ function Dashboard() {
           <li>
             <Link
               to="/user/my-assets"
-              className="flex items-center w-full gap-3 px-4 py-6 transition-colors duration-500 rounded-xl hover:bg-secondary"
+              className={`flex items-center w-full gap-3 px-4 py-6 transition-colors duration-500 rounded-xl hover:bg-secondary ${
+                pathname.includes("my-assets") ? "bg-secondary" : ""
+              }`}
             >
               <img src={assetIcon} alt="icon for dashboard" />
               <span>My Assets</span>
