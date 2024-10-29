9d64cceb1a9e2a86088a6f5330de13af1103ad77
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-29 12:14:36 +0100

    chore: update the searchComponent to handle search functionality and error

--- src/context/searchContext.jsx
+++ src/context/searchContext.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createContext } from "react";
 import PropTypes from "prop-types";
 import { useContext } from "react";
+import { useState } from "react";
+import { handleGetProductBySearch } from "../firebase/firestone";
 
 SearchProviderContext.propTypes = {
   children: PropTypes.node.isRequired,
@@ -11,7 +13,36 @@ SearchProviderContext.propType = {};
 const searchContext = createContext();
 
 export function SearchProviderContext({ children }) {
-  return <searchContext.Provider value={{}}>{children}</searchContext.Provider>;
+  const [product, setProduct] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchErr, setSeachErr] = useState("");
+
+  const handleSearch = async function (search) {
+    setSeachErr("");
+
+    if (!search) return setSeachErr("There is no search value");
+
+    try {
+      setIsSearching(true);
+      const data = await handleGetProductBySearch(Number(search));
+      if (data.docs.length === 0)
+        throw new Error("There is no product that match your search results");
+
+      data.forEach((docs) => setProduct(docs.data()));
+    } catch (error) {
+      setSeachErr(error.message);
+    } finally {
+      setIsSearching(false);
+    }
+  };
+
+  return (
+    <searchContext.Provider
+      value={{ product, isSearching, searchErr, handleSearch }}
+    >
+      {children}
+    </searchContext.Provider>
+  );
 }
 
 export function searchProvider() {
