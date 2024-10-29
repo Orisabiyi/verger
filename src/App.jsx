c763cfb5e9d50456c455818e43fb71d867c9ad0c
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-29 12:26:25 +0100

    refactor: update App.jsx to include the SearchProviderContext for search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,22 @@ import DashboardMenu from "./pages/DashboardMenu";
 import VerifyItem from "./pages/VerifyItem";
 import Assets from "./pages/Assets";
 import Homepage from "./pages/Homepage";
+import { SearchProviderContext } from "./context/searchContext";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/user" element={<Dashboard />}>
-          <Route index element={<DashboardMenu />} />
-          <Route path="dashboard" element={<DashboardMenu />} />
-          <Route path="verify-item" element={<VerifyItem />} />
-          <Route path="verify-item/:id" element={<VerifyItem />} />
-          <Route path="my-assets" element={<Assets />} />
-        </Route>
+        <SearchProviderContext>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/user" element={<Dashboard />}>
+            <Route index element={<DashboardMenu />} />
+            <Route path="dashboard" element={<DashboardMenu />} />
+            <Route path="verify-item" element={<VerifyItem />} />
+            <Route path="verify-item/:id" element={<VerifyItem />} />
+            <Route path="my-assets" element={<Assets />} />
+          </Route>
+        </SearchProviderContext>
       </Routes>
     </BrowserRouter>
   );
